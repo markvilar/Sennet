367fecb0d49ec626741bf9a25152c7e6208a5a75
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2021-10-17 16:34:46 +0200

    Conan - Add OpenGL system to conanfile.

--- scripts/build-debug.sh
+++ scripts/build-debug.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-mkdir -p build && cd build
-conan install .. -s build_type=Debug --profile=default
-cmake .. -G "Ninja" -DCMAKE_BUILD_TYPE=Debug
-cmake --build .
+
+BUILD_DIR="./build/"
+BUILD_TYPE="Debug"
+
+mkdir -p build
+conan install . --install-folder ${BUILD_DIR} \
+    --settings build_type=${BUILD_TYPE} \
+    --profile=default --build missing
+cmake -B ${BUILD_DIR} -G "Ninja" -D CMAKE_BUILD_TYPE=${BUILD_TYPE}
+cmake --build ${BUILD_DIR}
