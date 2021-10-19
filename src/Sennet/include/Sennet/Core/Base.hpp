c94eec331cb7fa32565dee168e9b2927746c3cf2
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2021-10-19 21:40:08 +0200

    Refactor platform and assertion macros from base.

--- src/Sennet/include/Sennet/Core/Base.hpp
+++ src/Sennet/include/Sennet/Core/Base.hpp
@@ -1,62 +1,15 @@
 #pragma once
 
-#include <memory>
-
-#if defined(SENNET_PLATFORM_LINUX)
-#elif defined(SENNET_PLATFORM_WINDOWS)
-#define _WIN32_WINNT 0x0601
-#include <asio.hpp>
-#include <windows.h>
-#elif defined(SENNET_PLATFORM_MACOS)
-#error "MACOS is not supported yet!"
-#elif defined(SENNET_PLATFORM_IOS)
-#error "IOS is not supported yet!"
-#else
-#error "Unknown build platform!"
-#endif
-
 #if defined(SENNET_DEBUG)
 #elif defined(SENNET_RELEASE)
 #else
 #error "Unknown build type!"
 #endif
 
-#if defined(SENNET_DEBUG)
-#if defined(SENNET_PLATFORM_WINDOWS)
-#define SENNET_DEBUGBREAK() __debugbreak()
-#elif defined(SENNET_PLATFORM_LINUX)
-#include <signal.h>
-#define SENNET_DEBUGBREAK() raise(SIGTRAP)
-#else
-#error "Platform doesn't support debugbreak yet!"
-#endif
-#define SENNET_ENABLE_ASSERTS
-#elif defined(SENNET_RELEASE)
-#define SENNET_DEBUGBREAK()
-#endif
+#include "Sennet/Core/Assert.hpp"
+#include "Sennet/Core/Platform.hpp"
 
-// TODO: Make macro able to take in no arguments except condition.
-#ifdef SENNET_ENABLE_ASSERTS
-#define SENNET_ASSERT(x, ...)                                                  \
-    {                                                                          \
-        if (!(x))                                                              \
-        {                                                                      \
-            SENNET_ERROR("Assertion failed: {0}", __VA_ARGS__);                \
-            SENNET_DEBUGBREAK();                                               \
-        }                                                                      \
-    }
-#define SENNET_CORE_ASSERT(x, ...)                                             \
-    {                                                                          \
-        if (!(x))                                                              \
-        {                                                                      \
-            SENNET_CORE_ERROR("Assertion failed: {0}", __VA_ARGS__);           \
-            SENNET_DEBUGBREAK();                                               \
-        }                                                                      \
-    }
-#else
-#define SENNET_ASSERT(x, ...)
-#define SENNET_CORE_ASSERT(x, ...)
-#endif
+#include <memory>
 
 #define BIT(x) (1 << x)
 
