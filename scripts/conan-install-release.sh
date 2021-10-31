9a22a505c3164fc9c2c48d2d05ccad6a2107ecc1
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2021-10-31 16:24:37 +0100

    Revert to C++17, and merge build and conan scripts.

--- scripts/conan-install-release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-BUILD_DIR="./build/"
-BUILD_TYPE="Debug"
-
-mkdir -p build
-conan install . --install-folder ${BUILD_DIR} \
-    --settings build_type=${BUILD_TYPE} \
-    --profile=default --build missing
