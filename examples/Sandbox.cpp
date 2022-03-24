de685f5f1681927e6e79dbc1538b31389c18aa1d
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2022-03-24 20:13:55 +0100

    Add Conan package support, as well as CMake install support.

--- examples/Sandbox.cpp
+++ examples/Sandbox.cpp
@@ -1,3 +1,35 @@
-#include "Pine/Pine.hpp"
+#include <chrono>
+#include <thread>
 
-int main(int argc, char** argv) { Pine::Log::Init(); }
+#include "Pine/Pine.hpp"
+#include "Pine/Experimental.hpp"
+
+static bool exitFlag = false;
+
+void signalHandler(const int signum) { exitFlag = true; }
+
+int main(int argc, char** argv) 
+{ 
+    signal(SIGINT, signalHandler);
+
+    Pine::Log::Init();
+
+    const auto host = "192.168.1.150";
+    const auto port = 4001;
+    Pine::UDP::ClientState client;
+    if (!Pine::Connect(client, host, port))
+    {
+        PINE_INFO("Not able to connect to server {0}:{1}", host, port);
+        return 1;
+    }
+
+    Pine::Send(client);
+    Pine::Receive(client);
+
+    while (!exitFlag)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+
+    Pine::Disconnect(client);
+}
--- scripts/cmake-format.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-find . -type f -name "CMakeLists.txt" -o -name "*.cmake" \
-    | xargs cmake-format -c .cmake-format.yaml -i 
-
-exit 0
