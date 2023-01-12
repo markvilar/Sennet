29346f214b4a5906ba759db6f52f9f10960e82b3
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2023-01-12 12:58:18 +0100

    Enable ImGui default profile.

--- src/core/src/gui/graphical_interface.cpp
+++ src/core/src/gui/graphical_interface.cpp
@@ -31,7 +31,6 @@ GraphicalInterface::GraphicalInterface(Window* window)
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
     io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
-    io.IniFilename = nullptr; // TODO: Add user profile.
 
     ImGui::StyleColorsDark();
 
