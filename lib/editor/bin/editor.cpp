327cdab4dbbd44c8c3014582648b7b256e5e0136
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2023-07-17 13:31:41 +0200

    Patch new project setup. Remove bin folder from gitignore and add the new editor executable.

---
+++ lib/editor/bin/editor.cpp
@@ -0,0 +1,31 @@
+#include "editor/editor.hpp"
+
+#include <filesystem>
+#include <memory>
+#include <string>
+
+#include <pine/pine.hpp>
+
+int main(int argc, char** argv)
+{
+    pine::EngineSpecs specs;
+    specs.working_directory = ".";
+    specs.name = "Editor";
+    specs.window_width = 1280;
+    specs.window_height = 720;
+    specs.start_maximized = true;
+    specs.vsync = true;
+    specs.resizable = true;
+    specs.enable_gui = true;
+    specs.fullscreen = true;
+
+    // Create application
+    pine::Editor editor;
+
+    // Initialize engine and run application
+    pine::Engine engine(specs);
+    //engine.init();
+    engine.run(editor);
+    
+    return 0;
+}
